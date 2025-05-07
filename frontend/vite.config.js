a0447d4f6571a2af2035bcd49eacfcf0775d5366
Pablo <pablo.silva@ahgora.com.br>
2025-05-07 15:53:18 -0300

    fix url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    // Configurar o histórico para suportar o modo SPA
+    historyApiFallback: true
   },
   build: {
     outDir: 'dist',
   },
+  // Adicionar a seguinte configuração para garantir que o router funcione em produção
+  base: '/',
 });
